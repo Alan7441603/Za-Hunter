75729b1caee93e42569b3e4e1db4471dc1358e9e
Alan Rivera <arivera@bsd220.org>
2024-02-21 09:31:46 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Alan Rivera on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
